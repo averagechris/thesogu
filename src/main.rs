11611d66d8769938170687c1aeb13e133c2786f0
mistahchris <mistahcummings@gmail.com>
2018-04-01 13:34:12 -0500

    redirect all port 80 requests to https

--- src/main.rs
+++ src/main.rs
@@ -14,24 +14,35 @@ fn index(_req: HttpRequest) -> Result<HttpResponse, Error> {
         .body(&html)?)
 }
 
+fn redirect_to_https(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Found()
+        .header("LOCATION", "https://thesogu.com")
+        .finish().unwrap()
+}
+
 fn main() {
     let sys = actix::System::new("thesogu");
     let mut ssl_builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
     ssl_builder
-        .set_private_key_file("/etc/letsencrypt/live/www.thesogu.com/privkey.pem", SslFiletype::PEM)
+        .set_private_key_file(
+            "/etc/letsencrypt/live/www.thesogu.com/privkey.pem",
+            SslFiletype::PEM,
+        )
         .unwrap();
-    ssl_builder.set_certificate_chain_file("/etc/letsencrypt/live/www.thesogu.com/fullchain.pem").unwrap();
+    ssl_builder
+        .set_certificate_chain_file("/etc/letsencrypt/live/www.thesogu.com/fullchain.pem")
+        .unwrap();
 
     let _ = HttpServer::new(|| Application::new().resource("/", |r| r.f(index)))
         .bind("0.0.0.0:443")
         .expect("Cannot bind to 443")
-        .start_ssl(ssl_builder).unwrap();
+        .start_ssl(ssl_builder)
+        .unwrap();
 
-    let _ = HttpServer::new(|| Application::new().resource("/", |r| r.f(index)))
+    let _ = HttpServer::new(|| Application::new().resource("/", |r| r.f(redirect_to_https)))
         .bind("0.0.0.0:80")
         .expect("Cannot bind to 80")
         .start();
 
     let _ = sys.run();
 }
-
